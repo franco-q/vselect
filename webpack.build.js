a6fd125f4a47adca2e6f99b1b81fdcf0ab857306
franco-q <franco.n.quintana@gmail.com>
2018-01-17 12:17:41 -0300

    webpack output library

--- webpack.build.js
+++ webpack.build.js
@@ -8,7 +8,7 @@ module.exports = {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
 		libraryTarget: 'umd',
-		library: 'vselect',
+		library: 'vslct',
 		umdNamedDefine: true
 	},
 	module: {
